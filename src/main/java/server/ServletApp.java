4340e3e5088aad83e860d691ec8503c1d1661a01
Aflatun Velibeyli <a.velibeyli6@gmail.com>
2020-05-13 03:04:52 +0400

    11'th requirement is done

---
+++ src/main/java/server/ServletApp.java
@@ -0,0 +1,18 @@
+package server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class ServletApp {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        ServletContextHandler handler = new ServletContextHandler();
+        TemplateEngine engine = TemplateEngine.folder("./templates");
+        handler.addServlet(new ServletHolder(new UsersServlet(engine)),"/users/*");
+        handler.addServlet(new ServletHolder(new StaticServlet()),"/static/*");
+        server.setHandler(handler);
+        server.start();
+        server.join();
+    }
+}
