f75908a8ae2bdd9015721068688c040d163576f6
Aflatun Velibeyli <a.velibeyli6@gmail.com>
2020-05-14 22:35:29 +0400

    nothing added yet since 16'th requirement

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -2,7 +2,7 @@ package entity;
 
 import lombok.*;
 
-@AllArgsConstructor
+
 @Getter
 @Setter
 @ToString
@@ -10,4 +10,12 @@ import lombok.*;
 public class User {
     private final String name;
     private final String imageURL;
+    private final int id;
+    private static int ids = 0;
+
+    public User(String name, String imageURL) {
+        this.name = name;
+        this.imageURL = imageURL;
+        this.id = ++ids;
+    }
 }
